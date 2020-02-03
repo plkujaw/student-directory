cc63a38770af41f79065c962e12127848ede7213
plkujaw <jawor.jakub@gmail.com>
2020-02-03 09:42:38 +0000

    Add students array and array printing/counting

--- students.rb
+++ students.rb
@@ -1,18 +1,25 @@
-student_count = 11
+# add students into an array
+students = [
+"Dr. Hannibal Lecter",
+"Darth Vader",
+"Nurse Ratchet",
+"Michael Corleone",
+"Alex DeLarge",
+"The Wicked Witch of the West",
+"Terminator",
+"Freddy Krueger",
+"The Joker",
+"Joffrey Baratheon",
+"Norman Bates"
+]
 
-# print the list of students
+#print the list of the students
 puts "The students of Villains Academy"
-puts "--------------"
-puts "Dr. Hannibal Lecter"
-puts "Dart Vader"
-puts "Nurse Ratchet"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+puts "--------------------------------"
+
+students.each do |student|
+  puts student
+end
+
 # print total number of students
-puts "Overall we have #{student_count} great students"
+puts "Overall we have #{students.count} great students"
