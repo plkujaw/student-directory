f1cd45c05cbc28889c8bd8e5f4a16915f3bc411b
plkujaw <jawor.jakub@gmail.com>
2020-02-03 09:16:18 +0000

    Added students count

--- students.rb
+++ students.rb
@@ -1,3 +1,4 @@
+# print the list of students
 puts "The students of Villains Academy"
 puts "--------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# print total number of students
+print "Overall we have "
+# print() doesn't add new line characters \n
+print 11
+puts " great students"
