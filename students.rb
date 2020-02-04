3644a40ad0541a05eb48739312c3a98cd91a56d4
plkujaw <jawor.jakub@gmail.com>
2020-02-04 10:57:08 +0000

    Add interactive menu

--- students.rb
+++ students.rb
@@ -1,18 +1,3 @@
-# add students data into an array of hashes
-# students = [
-# {name: "Dr. Hannibal Lecter", cohort: :november},
-# {name: "Darth Vader", cohort: :november},
-# {name: "Nurse Ratchet", cohort: :november},
-# {name: "Michael Corleone", cohort: :november},
-# {name: "Alex DeLarge", cohort: :november},
-# {name: "The Wicked Witch of the West", cohort: :november},
-# {name: "Terminator", cohort: :november},
-# {name: "Freddy Krueger", cohort: :november},
-# {name: "The Joker", cohort: :november},
-# {name: "Joffrey Baratheon", cohort: :november},
-# {name: "Norman Bates", cohort: :november}
-# ]
-
 # input students
 def input_students
   puts "Please enter the names of the students"
@@ -26,10 +11,20 @@ def input_students
     # add the student hash into the array
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
-    # get another nme of the student
+    # get another name of the student
     name = gets.chomp
   end
   students
+end
+
+# interactive menu
+def interactive_menu
+  loop do
+  # 1. print the menu and ask user what to do
+  # 2. read the input and save it to variable
+  # 3. do what the user has asked
+  # 4. repeat from step 1
+  end
 end
 
 # print header
