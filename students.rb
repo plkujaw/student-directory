6d89cb620f347fd0c38e434868a1104deeccb03f
plkujaw <jawor.jakub@gmail.com>
2020-02-04 10:13:27 +0000

    Add more students details and print them

--- students.rb
+++ students.rb
@@ -15,15 +15,23 @@
 
 # input students
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the name of the student"
   puts "To finish just hit return twice"
   # create an empty array
   students = []
-  # get the first name
-  name = gets.chomp
+  # get the students details
+  name = gets.chomp.to_s
+  puts "Please enter the students cohort"
+  cohort = gets.chomp.to_s
+  puts "Please enter the students age"
+  age = gets.chomp.to_i
+  puts "Please enter the students main power"
+  power = gets.chomp.to_s
+  puts "Please enter the students favourite colour"
+  colour = gets.chomp.to_s
   while !name.empty? do
     # add the student hash into the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: cohort, age: age, power: power, colour: colour}
     puts "Now we have #{students.count} students"
     # get another nme of the student
     name = gets.chomp
@@ -40,10 +48,8 @@ end
 # print the list of the students
 def print(students)
   students.each_with_index do |student, index|
-    if student[:name].length < 12
-      puts "#{(index + 1)}. #{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{(index + 1)}. #{student[:name]} (cohort: #{student[:cohort]}, age: #{student[:age]}, main power: #{student[:power]}, favourite colour: #{student[:colour]})"
     end
-  end
 end
 
 # print total number of students
