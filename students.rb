91770257a452eed47a821c188fa5256d12640301
plkujaw <jawor.jakub@gmail.com>
2020-02-04 11:10:27 +0000

    Code and test interactive menu

--- students.rb
+++ students.rb
@@ -19,14 +19,27 @@ end
 
 # interactive menu
 def interactive_menu
+  students = []
   loop do
-  # 1. print the menu and ask user what to do
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit"
-  # 2. read the input and save it to variable
-  selection = gets.chomp
-  # 3. do what the user has asked
+    # 1. print the menu and ask user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    # 2. read the input and save it to variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit # this will cause the program to terminate
+      else
+        puts "I don't know what you meant, try again"
+    end
   end
 end
 
@@ -49,8 +62,4 @@ def print_footer(names)
 end
 
 
-students = input_students
-
-print_header
-print(students) # passing students array of hashes as the method argument so students array = students method argument
-print_footer(students) # passing students array of hashes as the method argument so students array = names method argument
+interactive_menu
