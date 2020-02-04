43da9ca79aaac4feb364224f1757a6d4cbe56826
plkujaw <jawor.jakub@gmail.com>
2020-02-04 08:57:16 +0000

    Add printing students names start with letter

--- students.rb
+++ students.rb
@@ -40,7 +40,9 @@ end
 # print the list of the students
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{(index + 1)}. #{student[:name]} (#{student[:cohort]} cohort)"
+    if student[:name].start_with? "L"
+      puts "#{(index + 1)}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
